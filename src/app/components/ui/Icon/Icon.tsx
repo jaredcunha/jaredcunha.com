57498f5428c727d51dbee528a2f43b14a62c4ea6
Jared Cunha <jaredcunha@gmail.com>
2025-06-29 12:36:25 -0400

    add prev/next posts

--- src/app/components/ui/Icon/Icon.tsx
+++ src/app/components/ui/Icon/Icon.tsx
@@ -1,40 +1,40 @@
 interface IconProps {
-	icon: 'bars' | 'times';
-	className?: string;
+  icon: 'bars' | 'times';
+  className?: string;
 }
 
 export function Icon({ icon, className = '' }: IconProps) {
-	if (icon === 'bars') {
-		return (
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 448 512"
-				className={className}
-				aria-hidden="true"
-			>
-				<path
-					fill="currentColor"
-					d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
-				/>
-			</svg>
-		);
-	}
+  if (icon === 'bars') {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 448 512"
+        className={className}
+        aria-hidden="true"
+      >
+        <path
+          fill="currentColor"
+          d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"
+        />
+      </svg>
+    );
+  }
 
-	if (icon === 'times') {
-		return (
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 384 512"
-				className={className}
-				aria-hidden="true"
-			>
-				<path
-					fill="currentColor"
-					d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
-				/>
-			</svg>
-		);
-	}
+  if (icon === 'times') {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 384 512"
+        className={className}
+        aria-hidden="true"
+      >
+        <path
+          fill="currentColor"
+          d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+        />
+      </svg>
+    );
+  }
 
-	return null;
+  return null;
 }
